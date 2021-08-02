97399cc015355958ccd6dbef5ab784483be53790
jackykatae <jackykatae@users.noreply.github.com>
2021-08-02 21:02:36 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.c9ffe4bfa02124d15c7ea7f90bde6207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f181493ee8a37a95f8a062912b84f0b3",
+    "url": "https://jackykatae.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "d72314c67a9b4e69ddc9",
+    "url": "https://jackykatae.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "ba417aa7333b9dcebd4c",
+    "url": "https://jackykatae.github.io/task-management-frontend/static/js/2.8aa3a60b.chunk.js"
+  },
+  {
+    "revision": "d72314c67a9b4e69ddc9",
+    "url": "https://jackykatae.github.io/task-management-frontend/static/js/main.5b1c6931.chunk.js"
+  },
+  {
+    "revision": "823b571589e941f2e19a",
+    "url": "https://jackykatae.github.io/task-management-frontend/static/js/runtime~main.657a0d97.js"
+  }
+]);
